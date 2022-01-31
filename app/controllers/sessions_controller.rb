b2129f48454df1d6dd7bdd3e5de310b22453a9b6
Chelsey T <73909880+CLTsolutions@users.noreply.github.com>
2022-01-30 21:19:53 -0500

    Add sign in routes #17
    - Add sign in to routes
    - Create sign in form
    - Add logic to sessions controller to find user by email and authenticate password

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,18 @@
 class SessionsController < ApplicationController
+  def new; end
+
+  def create
+    user = User.find_by(email: params[:email])
+    if user.present? && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to root_path, notice: 'Logged in successfully'
+    else
+      flash[:alert] = 'Invalid email or password'
+      # renders new action (sessions => new.html.erb)
+      render :new
+    end
+  end
+
   def destroy
     # getting rid of id (won't exist anymore)
     session[:user_id] = nil
