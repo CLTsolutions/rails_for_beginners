2ef4e0d50c3eb12520b716983e614a2ae8e8b5cc
Chelsey T <73909880+CLTsolutions@users.noreply.github.com>
2022-03-10 21:35:18 -0500

    Finish password reset #21

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -5,11 +5,32 @@ class PasswordResetsController < ApplicationController
   def create
     @user = User.find_by(email: params[:email])
 
-    if @user.present?
-      # Send email
-      PasswordMailer.with(user: @user).reset.deliver_now
-    end
+    # Send email (see notes.md)
+    PasswordMailer.with(user: @user).reset.deliver_now if @user.present?
 
     redirect_to root_path, notice: 'If an account with that email was found, we have sent a link to reset your password.'
+  end
+
+  def edit
+    @user = User.find_signed!(params[:token], purpose: 'password_reset') # bang throws exception
+    # used binding.irb to show @user is nil
+    # binding.irb
+  rescue ActiveSupport::MessageVerifier::InvalidSignature
+    redirect_to sign_in_path, alert: 'Your token has expired. Please try again.'
+  end
+
+  def update
+    @user = User.find_signed!(params[:token], purpose: 'password_reset') # bang throws exception
+    if @user.update(password_params)
+      redirect_to sign_in_path, notice: 'Your password was reset successfully. Please sign in.'
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def password_params
+    params.require(:user).permit(:password, :password_confirmation)
   end
 end
